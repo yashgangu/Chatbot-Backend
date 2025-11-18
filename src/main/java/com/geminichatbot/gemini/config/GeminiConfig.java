152c6e8d2a17dc3384972d8a4ce811ae116828a0
yashgangu <gangurdeyash122003.com>
2025-11-18 15:33:13 +0530

    Fix @Value import and add API key injection

--- src/main/java/com/geminichatbot/gemini/config/GeminiConfig.java
+++ src/main/java/com/geminichatbot/gemini/config/GeminiConfig.java
@@ -2,9 +2,11 @@ package com.geminichatbot.gemini.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
-import com.google.api.client.util.Value;
+
 import com.google.genai.Client;
+
 
 @Configuration
 public class GeminiConfig {
@@ -14,6 +16,8 @@ public class GeminiConfig {
 
 	@Bean
 	public Client geminiClient() {
+		System.out.println("API KEY = " + apiKey);
+
 		return new Client.Builder()
 				.apiKey(apiKey)
 				.build();
