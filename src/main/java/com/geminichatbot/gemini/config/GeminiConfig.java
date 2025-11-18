2f7a2e265b2cd646d1ba75c07a5f6a40df11e1f0
yashgangu <gangurdeyash122003.com>
2025-11-18 14:58:58 +0530

    Fix Gemini Client config and update API integration

--- src/main/java/com/geminichatbot/gemini/config/GeminiConfig.java
+++ src/main/java/com/geminichatbot/gemini/config/GeminiConfig.java
@@ -3,13 +3,19 @@ package com.geminichatbot.gemini.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.api.client.util.Value;
 import com.google.genai.Client;
 
 @Configuration
 public class GeminiConfig {
+	
+	 @Value("${GOOGLE_API_KEY}")
+	    private String apiKey;
 
 	@Bean
 	public Client geminiClient() {
-		return new Client();
+		return new Client.Builder()
+				.apiKey(apiKey)
+				.build();
 	} 
 }
