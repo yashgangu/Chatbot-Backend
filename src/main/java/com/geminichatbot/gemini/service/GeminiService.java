840f01b9d62e2f4fce22f9476c62b3cbac4b1493
yashgangu <gangurdeyash122003.com>
2025-11-21 14:42:45 +0530

    Updated GeminiService to async + improved response

--- src/main/java/com/geminichatbot/gemini/service/GeminiService.java
+++ src/main/java/com/geminichatbot/gemini/service/GeminiService.java
@@ -15,18 +15,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GeminiService {
 
-	private final Client client;
-    
-   
-    @Async 
-	public CompletableFuture<String> askGemini(String prompt) {
+    private final Client client;
+
+    @Async
+    public CompletableFuture<String> askGemini(String prompt) {
+
+        String finalPrompt =
+            "You are an IRCTC Assistant. Answer in short, clear bullet points. " +
+            "Avoid long paragraphs. Give exact train names, timings, and steps.\n\n" +
+            "User: " + prompt;
+
         System.out.println("Prompt received in thread: " + Thread.currentThread().getName());
-        
-		GenerateContentResponse response = client.models.generateContent("gemini-2.5-flash", prompt, null);
-		
-		System.out.println("Response generated in thread: " + Thread.currentThread().getName());
-        
-        
-		return CompletableFuture.completedFuture(response.text());
-	}
+
+        GenerateContentResponse response = 
+            client.models.generateContent("gemini-2.5-flash", finalPrompt, null);
+
+        System.out.println("Response generated in thread: " + Thread.currentThread().getName());
+
+        return CompletableFuture.completedFuture(response.text());
+    }
 }
